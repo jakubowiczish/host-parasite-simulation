1151ff887aa7a57fbf621bc85a308e09ee9f8345
Rafał Lisak <rafal.lisak@appliscale.io>
2021-03-18 01:09:32 +0100

    Die after hunger

--- test-covid-simulation.py
+++ test-covid-simulation.py
@@ -15,7 +15,7 @@ space = pymunk.Space()
 
 population = 2
 food_init_number = 20
-
+DIE_TYPE_COLLISION = population + food_init_number + 10
 recovery_time = 300  # dependent on fps
 
 
@@ -33,51 +33,51 @@ class Host:
         self.infected_time = 0
         self.infected = False
         self.recovered = False
+        self.health = 100
         space.add(self.body, self.shape)
 
     def pass_time(self):
-        if self.infected:
-            self.infected_time += 1
-        if self.infected_time >= recovery_time:
-            self.infected = False
-            self.recovered = True
-            self.shape.collision_type = population + 2
+        print(self.health)
+        if self.health > 0:
+            self.health -= 1 / FPS
+        else:
+            self.shape.collision_type = DIE_TYPE_COLLISION
+            self.die()
+
+    def die(self):
+        self.body.velocity = 0, 0
 
     def draw(self):
         x, y = self.body.position
-        if self.infected:
+        if self.health == 0:
             pygame.draw.circle(display, (255, 0, 0), (int(x), int(y)), 10)
-        elif self.recovered:
-            pygame.draw.circle(display, (0, 0, 255), (int(x), int(y)), 10)
         else:
             pygame.draw.circle(display, (255, 255, 255), (int(x), int(y)), 10)
-
-    def infect(self, space=0, arbiter=0, data=0):
-        self.infected = True
-        self.shape.collision_type = population + 1
 
     def eat(self, space, arbiter, data):
         food = data['food']
         foods = data['foods']
         if food in foods:
             foods.remove(food)
+            self.health += food.nutrition
         return False
 
     def find_nearest_food(self, foods):
-        vector_to_food = random.uniform(-100, 100), random.uniform(-100, 100)
-        if not foods:
-            return vector_to_food
-        min_length = 10000000
-        speed = 80
-        for food in foods:
-            vector = np.array([food.x - self.body.position.x, food.y - self.body.position.y])
-            length = np.linalg.norm(vector)
-            if length < min_length:
-                min_length = length
-                versor = speed * vector / length
-                vector_to_food = versor.tolist()
+        if self.health > 0:
+            vector_to_food = random.uniform(-100, 100), random.uniform(-100, 100)
+            if not foods:
+                return vector_to_food
+            min_length = 10000000
+            speed = 80
+            for food in foods:
+                vector = np.array([food.x - self.body.position.x, food.y - self.body.position.y])
+                length = np.linalg.norm(vector)
+                if length < min_length:
+                    min_length = length
+                    versor = speed * vector / length
+                    vector_to_food = versor.tolist()
 
-        self.body.velocity = vector_to_food
+            self.body.velocity = vector_to_food
 
 
 class Wall:
@@ -98,6 +98,7 @@ class Food:
         self.shape.density = 1
         self.shape.elasticity = 1
         self.shape.collision_type = i
+        self.nutrition = 4
         space.add(self.body, self.shape)
 
     def draw(self):
@@ -109,7 +110,6 @@ def game():
     balls = [Host(random.randint(0, size_x), random.randint(0, size_y), i + 1) for i in range(population)]
     foods = [Food(random.randint(0, size_x), random.randint(0, size_y), i + 1) for i in
              range(population, population + food_init_number)]
-
     for ball in balls:
         for food in foods:
             handler = space.add_collision_handler(ball.shape.collision_type, food.shape.collision_type)
